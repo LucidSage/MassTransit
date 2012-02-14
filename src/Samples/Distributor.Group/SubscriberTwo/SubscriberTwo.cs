020ddfd41a77d76c356653cd04c73714c6633d85
Darren Boon <darren.boon@rogers.com>
2012-02-14 15:39:50 -0500

    Adding group distributor sample

---
+++ src/Samples/Distributor.Group/SubscriberTwo/SubscriberTwo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MassTransit;
+using Messages;
+using MassTransit.Distributor.Grouping;
+
+namespace SubscriberTwo
+{
+	class SubscriberTwo
+	{
+		static void Main(string[] args)
+		{
+			using (var bus = ServiceBusFactory.New(sbc =>
+			{
+				sbc.UseMsmq();
+				sbc.VerifyMsmqConfiguration();
+				sbc.SetNetwork("mt_group_demo");
+				sbc.UseMulticastSubscriptionClient();
+				sbc.UseGroupWorker<YourMessage>("two");
+
+				sbc.ReceiveFrom("msmq://localhost/mt_group_events_sub_two");
+
+			}))
+			{
+				bus.SubscribeHandler<YourMessage>(msg =>
+				{
+					ConsoleOut(msg.Text);
+				});
+
+				ConsoleOut("SubscriberTwo Ready.");
+
+				// Sit and wait
+				Console.ReadLine();
+			}
+		}
+
+		static Object _lock = new Object();
+
+		private static void ConsoleOut(string inLine)
+		{
+			lock (_lock)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine(inLine);
+				Console.ResetColor();
+			}
+		}
+	}
+}
